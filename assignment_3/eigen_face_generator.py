e9962372efc628a905468bf419b87f8665a0954d
RushilDaya <rushildayabme@gmail.com>
2019-03-21 16:38:02 +0100

    removed old files

--- assignment_3/eigen_face_generator.py
+++
@@ -1,9 +0,0 @@
-from mixins import getAllImagePaths, getImages, computeEigenFaces, saveModel
-
-PATH_TO_PROCESSED = './data/extracted_faces/'
-
-if __name__ =='__main__':
-    allImagePaths = getAllImagePaths(PATH_TO_PROCESSED, recursive=True)
-    allFaces = getImages(allImagePaths)
-    model = computeEigenFaces(allFaces)
-    saveModel(model)
