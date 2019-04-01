49454a95ba841de759d8a58c085bf89fea8736d6
RushilDaya <rushildayabme@gmail.com>
2019-04-01 08:39:23 +0200

    added framework code

---
+++ assignment_3/final.py
@@ -0,0 +1,35 @@
+# this is a step by step file which - 
+# produces the serial movie
+# assumes the following:
+#   1 - the computeEigenFaces.py has been run
+#   2 - classification.py has been run
+
+# A - Show some of the raw faces
+
+# B - explain we use haar cascades to find faces
+
+# C - after splitting randomly mix up into training and test set
+
+# D - Show the mean face
+
+# E - show some of the eigen-faces after PCA
+
+# F - show the size of the eigenvalues (indicates set variance)
+
+# G - show the plot of generalized reconstruction error as a means 
+#     to determine the number of PCA components we need
+
+# H - try somehow to show a clustering of faces along eigen axes
+
+# I - take a few faces and draw them with more and fewer eigenfaces
+
+# J - explain briefly the 3 classification methods
+
+# J1 - we must show the new faces in terms of their PCA somehow ( show its the same)
+
+# K - show the accuracy face and accuracy numbers ( for knn show nearest faces)
+
+# L - repeat on more difficult ( non- representitive faces )
+
+
+
